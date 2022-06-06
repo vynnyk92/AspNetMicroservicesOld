3128f1ba919a0633a7e31ca30b1dfd06fe43b2bc
Pylyp <pylyp.vynnykov@justeattakeaway.com>
2022-06-06 22:34:11 +0300

    add discounts

---
+++ src/Services/Discount/Discount.API/Controllers/DiscountController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+using Discount.API.Entities;
+using Discount.API.Repositories;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace Discount.API.Controllers
+{
+    [ApiController]
+    [Route("/api/v1/[controller]")]
+    public class DiscountController : ControllerBase
+    {
+        private readonly IDiscountRepository _discountRepository;
+
+        public DiscountController(IDiscountRepository discountRepository)
+        {
+            _discountRepository = discountRepository;
+        }
+
+        [HttpGet("{productName}")]
+        [ProducesResponseType(typeof(Coupon), (int)HttpStatusCode.OK)]
+        [ProducesResponseType((int)HttpStatusCode.NotFound)]
+        public async Task<IActionResult> GetDiscount(string productName)
+        {
+            var coupon = await _discountRepository.GetDiscount(productName);
+            return coupon == null ? NotFound() : Ok(coupon);
+        }
+
+        [HttpDelete("{id}")]
+        [ProducesResponseType((int)HttpStatusCode.NoContent)]
+        public async Task<IActionResult> DeleteDiscount(int id)
+        {
+            var result = await _discountRepository.DeleteDiscount(id);
+            return result ? NoContent() : BadRequest();
+        }
+    }
+}
---
+++ src/Services/Discount/Discount.API/Repositories/IDiscountRepository.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Dapper;
+using Discount.API.Entities;
+using Microsoft.Extensions.Configuration;
+using Npgsql;
+
+namespace Discount.API.Repositories
+{
+    public interface IDiscountRepository
+    {
+        Task<Coupon> GetDiscount(string prodcutName);
+        Task<bool> CreateDiscount(Coupon coupon);
+        Task<bool> UpdateDiscount(Coupon coupon);
+        Task<bool> DeleteDiscount(int couponId);
+    }
+
+    public class DiscountRepository : IDiscountRepository
+    {
+        private readonly string _connectionString;
+
+        public DiscountRepository(IConfiguration configuration)
+        {
+            _connectionString = configuration.GetConnectionString("DiscountDb");
+        }
+
+        public async Task<Coupon> GetDiscount(string prodcutName)
+        {
+            using (var connection = new NpgsqlConnection(_connectionString))
+            {
+                var result = await connection.QueryFirstOrDefaultAsync<Coupon>(
+                    $"SELECT * FROM Coupon WHERE ProductName = @ProdcutName", new
+                    {
+                        ProdcutName = prodcutName
+                    });
+
+                return result;
+            }
+        }
+
+        public Task<bool> CreateDiscount(Coupon coupon)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> UpdateDiscount(Coupon coupon)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> DeleteDiscount(int couponId)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
